af500101a8bb39796e52a3b477a427626c70a2df
ly1412 <1079752@qq.com>
2024-02-25 19:00:01 +0800

    test data frame

--- q-learn/test/test_data_frame.py
+++ q-learn/test/test_data_frame.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 if __name__ == '__main__':
     q_table = pd.DataFrame(columns=list(range(4)), dtype=np.float64)
-    q_table = q_table.append(
+    print(q_table.columns)
+    q_table = q_table._append(
         pd.Series(
             [0] * 4,
             index=q_table.columns,
